82a163a7c66c8d515f985a99a37be8416ee8d731
Paulo Sergio <paulosergio_gyn@hotmail.com>
2024-03-28 11:20:24 -0300

    Crud de tarefas

---
+++ SistemasDeTarefas/Repositorios/UsuarioRepositorio.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SistemasDeTarefas.Data;
+using SistemasDeTarefas.Models;
+using SistemasDeTarefas.Repositorios.Interfaces;
+
+namespace SistemasDeTarefas.Repositorios
+{
+    public class UsuarioRepositorio : IUsuarioRepositorio
+    {
+        private readonly SistemaTarefasDBContex _dbContex;
+        public UsuarioRepositorio(SistemaTarefasDBContex sistemasTarefasDBContex)
+        {
+            _dbContex = sistemasTarefasDBContex;
+        }
+        public async Task<UsuarioModel> BuscarPorId(int id)
+        {
+            return await _dbContex.Usuarios.FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task<List<UsuarioModel>> BuscarTodosUsuarios()
+        {
+            return await _dbContex.Usuarios.ToListAsync();
+        }
+
+         public async Task<UsuarioModel> Adicionar(UsuarioModel usuario)
+        {
+            await _dbContex.Usuarios.AddAsync(usuario);
+            await _dbContex.SaveChangesAsync();
+            return usuario;
+        }
+        public async Task<UsuarioModel> Atualizar(UsuarioModel usuario, int id)
+        {
+            UsuarioModel usuarioPorId = await BuscarPorId(id);
+            if(usuarioPorId == null)
+            {
+                throw new Exception($"Usuário para o ID: {id} não foi encontrado.");
+            }
+
+            usuarioPorId.Nome = usuario.Nome;
+            usuarioPorId.Email = usuario.Email;
+
+            _dbContex.Update(usuarioPorId);
+            await _dbContex.SaveChangesAsync();
+
+            return usuarioPorId;
+        }
+
+
+        public async Task<bool> Apagar(int id)
+        {
+            UsuarioModel usuarioPorId = await BuscarPorId(id);
+            if (usuarioPorId == null)
+            {
+                throw new Exception($"Usuário para o ID: {id} não foi encontrado.");
+            }
+            _dbContex.Remove(usuarioPorId);
+            _dbContex.SaveChanges();
+
+            return true;
+        }
+
+     
+        
+    }
+}
